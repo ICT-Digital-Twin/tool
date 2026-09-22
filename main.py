cea04be437217e3aa425c80f9e2260d1f72f2924
L00196745 <L00196745@atu.ie>
2026-09-22 17:59:53 +0100

    updates

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+ICT Digital Twin Tool
+main.py
+Peter Drinkwater
+L00196745
+2026
+"""
+
+from modules.filemanager.filemanager import filemanager as fm, filename
+from tkinter import Tk, filedialog
+import yaml
+# for testing
+# from pprint import pprint
+
+def main():
+    # select a room layout file
+    room_layout_file = filedialog.askopenfilename(
+        title="Select a file",
+        filetypes=[
+            ("YAML files", "*.yaml")
+        ]
+    )
+
+    with open(room_layout_file, "r") as f:
+        room_layout_data = yaml.safe_load(f)
+    
+    room_width = room_layout_data["room"]["width"]
+    room_length = room_layout_data["room"]["length"]
+    room_height = room_layout_data["room"]["height"]
+    power_feed_a_voltage = room_layout_data["power"]["feed_a_voltage"]
+    power_feed_a_capacity = room_layout_data["power"]["feed_a_capacity"]
+    power_feed_b_voltage = room_layout_data["power"]["feed_b_voltage"]
+    power_feed_b_capacity = room_layout_data["power"]["feed_b_capacity"]
+
+if __name__ == "__main__":
+    main()
--- modules/filemanager/filemanager.py
+++ modules/filemanager/filemanager.py
@@ -1,1 +1,22 @@
-print("File Manager")
+"""
+Docstring for filemanager
+This will load files and create and update temp files
+"""
+
+from fileinput import filename
+from pathlib import Path
+from isort import file
+import pandas as pd
+
+class filemanager:
+
+    def load(self, filepath):
+
+        file = Path(filepath)
+
+        if file.suffix == ".yaml":
+            return pd.read_xml(file)
+
+        raise ValueError(
+        f"Unsupported file type: {file.suffix}"
+        )
