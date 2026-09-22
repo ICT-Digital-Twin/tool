7cccbd9020a4d3255fca006eccba57c8912816f4
L00196745 <L00196745@atu.ie>
2026-09-22 21:53:08 +0100

    more tweaks

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ PD
 2026
 """
 from modules.filemanager.filemanager import loadroom, loadassets
+from modules.display.display import brownian_motion 
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates.default = "plotly_dark"
+import pandas as pd
+
 # for testing: from pprint import pprint
+
+# bg_colour = "dimgray"
 
 def main():
     # select a room layout file
@@ -26,6 +34,52 @@ def main():
     asset_data = loadassets()
     print(asset_data)
 
+    # display the room layout and asset data
+    # test from plotly.com
+
+    dates = pd.date_range('2012-01-01', '2013-02-22')
+    T = (dates.max()-dates.min()).days / 365
+    N = dates.size
+    start_price = 100
+    y = brownian_motion(T, N, sigma=0.1, S0=start_price)
+    z = brownian_motion(T, N, sigma=0.1, S0=start_price)
+
+    fig = go.Figure(data=go.Scatter3d(
+        x=dates, y=y, z=z,
+        marker=dict(
+            size=4,
+            color=z,
+            colorscale='Viridis',
+        ),
+        line=dict(
+            color='darkblue',
+            width=2
+        )
+    ))
+
+    fig.update_layout(
+        width=800,
+        height=700,
+        autosize=False,
+        scene=dict(
+            camera=dict(
+                up=dict(
+                    x=0,
+                    y=0,
+                    z=1
+                ),
+                eye=dict(
+                    x=0,
+                    y=1.0707,
+                    z=1,
+                )
+            ),
+            aspectratio = dict( x=1, y=1, z=0.7 ),
+            aspectmode = 'manual'
+        ),
+    )
+
+    fig.show(config={"displayModeBar": False})
 
 if __name__ == "__main__":
     main()
