f845a0d9e0cf5c897a4db1a5c8affd35d027e011
L00196745 <L00196745@atu.ie>
2026-09-24 10:09:50 +0100

    updates

--- main.py
+++ main.py
@@ -66,54 +66,81 @@ def main():
 # this will display the room layout and asset data
     # but it's just a test from plotly.com
 
-    dates = pd.date_range('2012-01-01', '2013-02-22')
-    T = (dates.max()-dates.min()).days / 365
-    N = dates.size
-    start_price = 100
-    y = brownian_motion(T, N, sigma=0.1, S0=start_price)
-    z = brownian_motion(T, N, sigma=0.1, S0=start_price)
+    # dates = pd.date_range('2012-01-01', '2013-02-22')
+    # T = (dates.max()-dates.min()).days / 365
+    # N = dates.size
+    # start_price = 100
+    # y = brownian_motion(T, N, sigma=0.1, S0=start_price)
+    # z = brownian_motion(T, N, sigma=0.1, S0=start_price)
 
-    fig = go.Figure(data=go.Scatter3d(
-        x=dates, y=y, z=z,
-        marker=dict(
-            size=4,
-            color=z,
-            colorscale='Viridis',
-        ),
-        line=dict(
-            color='darkblue',
-            width=2
-        )
-    ))
+    # fig = go.Figure(data=go.Scatter3d(
+    #     x=dates, y=y, z=z,
+    #     marker=dict(
+    #         size=4,
+    #         color=z,
+    #         colorscale='Viridis',
+    #     ),
+    #     line=dict(
+    #         color='darkblue',
+    #         width=2
+    #     )
+    # ))
 
-    fig.update_layout(
-        width=800,
-        height=700,
-        autosize=False,
-        scene=dict(
-            camera=dict(
-                up=dict(
-                    x=0,
-                    y=0,
-                    z=1
+    # fig.update_layout(
+    #     width=800,
+    #     height=700,
+    #     autosize=False,
+    #     scene=dict(
+    #         camera=dict(
+    #             up=dict(
+    #                 x=0,
+    #                 y=0,
+    #                 z=1
+    #             ),
+    #             eye=dict(
+    #                 x=0,
+    #                 y=1.0707,
+    #                 z=1,
+    #             )
+    #         ),
+    #         aspectratio = dict( x=1, y=1, z=0.7 ),
+    #         aspectmode = 'manual',
+    #         dragmode = 'turntable'
+    #     ),
+    # )
+
+    # fig.show(config={"displayModeBar": False})
+
+    # fig.write_html(f"{output_dir}/testoutput.html")
+    # fig.write_image(f"{output_dir}/testoutput.png")
+
+    fig = go.Figure()
+    
+    for rack in sorted(asset_data["RACK"].unique()):
+    
+        rack_df = asset_data[asset_data["RACK"] == rack]
+    
+        fig.add_trace(
+                go.Scatter(
+                x=rack_df["RACK_UNIT"],
+                y=[rack] * len(rack_df),
+                mode="markers",
+                marker=dict(
+                    size=rack_df["SIZE"] * 12
                 ),
-                eye=dict(
-                    x=0,
-                    y=1.0707,
-                    z=1,
-                )
-            ),
-            aspectratio = dict( x=1, y=1, z=0.7 ),
-            aspectmode = 'manual',
-            dragmode = 'turntable'
-        ),
+                name=rack
+            )
+        )
+    
+    fig.update_layout(
+        template="plotly_white",
+        height=700,
+        hovermode="closest"
     )
-
-#    fig.update_layout(scene={"dragmode": "turntable"})
-    fig.show(config={"displayModeBar": False})
-
     fig.write_html(f"{output_dir}/testoutput.html")
     fig.write_image(f"{output_dir}/testoutput.png")
+
+    
 
 if __name__ == "__main__":
     main()
